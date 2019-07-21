d46a30a58f40c8e3c3149862ffc01229db186766
Dan Fitz <dan.fitz@pm.me>
2019-07-21 03:17:23 -0400

    Fixed overflow issue on mobile

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
+      <div className="appComponent">
         <Header title="Polar News" />
         <NewsFeed />
         <Footer attributions={[{name: "News API", href: "https://newsapi.org/docs"}, {name: "Aylien", href: "https://docs.aylien.com/textapi"}]} />
