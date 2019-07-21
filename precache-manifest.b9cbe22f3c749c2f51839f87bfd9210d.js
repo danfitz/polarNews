866a2bcd3699f4ca2bb2bb20d16800e9198a2afb
Dan Fitz <dan.fitz@pm.me>
2019-07-21 03:06:24 -0400

    Updates

---
+++ precache-manifest.b9cbe22f3c749c2f51839f87bfd9210d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63fae26dcfeba53d29aff95719ce748d",
+    "url": "/polarNews/index.html"
+  },
+  {
+    "revision": "fcd903bf4063e4c514f1",
+    "url": "/polarNews/static/css/main.a63dc351.chunk.css"
+  },
+  {
+    "revision": "6637b311ea17f8f176d1",
+    "url": "/polarNews/static/js/2.9459666e.chunk.js"
+  },
+  {
+    "revision": "fcd903bf4063e4c514f1",
+    "url": "/polarNews/static/js/main.3e6c009a.chunk.js"
+  },
+  {
+    "revision": "699a2e710bda9a1da91c",
+    "url": "/polarNews/static/js/runtime~main.6d786579.js"
+  },
+  {
+    "revision": "f825cd408109097a065aaab33c29a005",
+    "url": "/polarNews/static/media/fullEarth.f825cd40.png"
+  }
+]);
